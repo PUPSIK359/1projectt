09517b4274432bee8700f09c17a84a913d9f8578
StarostinAlex <s.starostin2007@mail.ru>
2026-09-22 11:07:02 +0300

    задание 5

--- src/LAB1/t3.java
+++ src/LAB1/t3.java
@@ -1,23 +1,22 @@
 package LAB1;
 
 import java.util.Arrays;
+//зд3 создаем мссив инициализаице как в ис и в цикле фор считаем сумму и ср ариф
 
-// Задание 3: массив задаётся инициализацией (как в Си),
-// в цикле for считаем сумму и среднее арифметическое.
 public class t3 {
     public static void main(String[] args) {
-        int[] numbers = {5, 12, -3, 8, 20, 7, 1}; // инициализация массива
+        int[] numbers = {5, 12, -3, 8, 20, 7, 1};
 
         int sum = 0;
         for (int i = 0; i < numbers.length; i++) {
-            sum += numbers[i]; // то же самое, что sum = sum + numbers[i]
+            sum += numbers[i];
         }
 
-        // (double) нужен, чтобы деление было дробным, а не целочисленным
-        double average = (double) sum / numbers.length;
 
-        System.out.println("Массив: " + Arrays.toString(numbers));
-        System.out.println("Сумма элементов: " + sum);
-        System.out.printf("Среднее арифметическое: %.2f%n", average);
+        double average = (double) sum / numbers.length;//дабл нужен для дроб деления не целочисл
+
+        System.out.println("массив: " + Arrays.toString(numbers));
+        System.out.println("сумма элементов: " + sum);
+        System.out.println("Среднее арифметическое: " + average);
     }
 }
